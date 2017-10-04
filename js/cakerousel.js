034a9a2a7ef205277b683d7f6a1e351360afee30
abelardogg <abelardogy@gmail.com>
2017-10-04 08:54:24 -0600

    loop + auto play :cyclone:

--- js/cakerousel.js
+++ js/cakerousel.js
@@ -1,39 +1,56 @@
 $(document).ready(function(){
+
 var currentPos = 0;
 var currentPosEnd = 992;
 var totalWidth = getTotalWidth();
 var speedScroll = 400;
 var scrollAmount = 992;
+var firstSlice = $('.cakerousel-element')[0];
+var lastSlice = $('.cakerousel-element')[($('.cakerousel-element').length-1)];
+		 
 
 $('#pos-value-l').text(currentPos);
 $('#pos-value-r').text(currentPosEnd);
 $('#width-value').text(totalWidth);
 $('#img-qty').text(getQuantityCarouselElements());
 
-
-
-$(".cakerousel-control-l").click(function(){
-	if(currentPos>0){
-		currentPosEnd-=scrollAmount;
-		currentPos-=scrollAmount;
-    $(".cakerousel-elements").animate({scrollLeft:currentPos},speedScroll);
-	} else{
-		jumpToLast();
-	}
-}); 
+$(document).ready(function(){
+	if($('.cakerousel-slider').hasClass('auto')){
+		var tid = setInterval(nextSlice, 1500);
+	 }
+});
 
 $(".cakerousel-control-r").click(function(){
+ nextSlice();
+});
+
+$(".cakerousel-control-l").click(function(){
+	previousSlice();
+}); 
+
+function nextSlice(){
 	if(currentPos<totalWidth && currentPosEnd+scrollAmount<totalWidth){
 		 currentPosEnd+=scrollAmount;
 		 currentPos+=scrollAmount;
     $(".cakerousel-elements").animate({scrollLeft:currentPos},speedScroll);
-		 } else{
-			 jumpToFirst()
-		 }
-});
+	 } else if($('.cakerousel-slider').hasClass('loop')) {
+		 fromLastToFirst();
+	 } else {
+		 jumpToFirst()
+	 }	
+}
 
-
-
+function previousSlice(){
+	if(currentPos>0){
+		currentPosEnd-=scrollAmount;
+		currentPos-=scrollAmount;
+    $(".cakerousel-elements").animate({scrollLeft:currentPos},speedScroll);
+	} else if($('.cakerousel-slider').hasClass('loop')) {
+					 fromFirstToLast()
+ 	} else {
+			jumpToLast();
+	}	
+}
 
 function jumpToLast(){
 	currentPos=(scrollAmount*(getQuantityCarouselElements()-1));
@@ -57,8 +74,38 @@ function getTotalWidth(){
 		totalWidth+=$($('.cakerousel-element')[i]).width()
 	}
 	
-	console.log('total:',totalWidth );
+	// console.log('total:',totalWidth );
 	return totalWidth+80;
+}
+
+function fromLastToFirst(){
+	$(firstSlice).insertAfter(lastSlice);
+	currentPosEnd-=scrollAmount;
+	currentPos-=scrollAmount;
+  $(".cakerousel-elements").animate({scrollLeft:currentPos},0);
+	firstSlice = $('.cakerousel-element')[0];
+	lastSlice = $('.cakerousel-element')[($('.cakerousel-element').length-1)];
+	currentPosEnd+=scrollAmount;
+	currentPos+=scrollAmount;
+  $(".cakerousel-elements").animate({scrollLeft:currentPos},speedScroll);
+	console.log($('.cakerousel-element'));
+}
+
+function fromFirstToLast(){
+	//move the last image to the first place
+	$(lastSlice).insertBefore(firstSlice);
+	//moves the slider to the old first image
+	currentPosEnd+=scrollAmount;
+	currentPos+=scrollAmount;
+  $(".cakerousel-elements").animate({scrollLeft:currentPos},0);
+	//reads who is the new first element and who is the new last
+	firstSlice = $('.cakerousel-element')[0];
+	lastSlice = $('.cakerousel-element')[($('.cakerousel-element').length-1)];
+	//perform the scroll
+	currentPosEnd-=scrollAmount;
+	currentPos-=scrollAmount;
+  $(".cakerousel-elements").animate({scrollLeft:currentPos},speedScroll);
+	console.log($('.cakerousel-element'));
 }
 
 //testing stuff
@@ -68,4 +115,7 @@ $('button').on('click', function(){
 	$('#pos-value-l').text(currentPos)
 	$('#pos-value-r').text(currentPosEnd)
 });
+
+
+
 });
