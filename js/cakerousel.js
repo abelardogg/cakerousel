649c25b48e17878cc09bc6663a9d2fa4c29d02bd
abelardogg <abelardogy@gmail.com>
2017-10-06 10:16:28 -0600

    autoplay on loads

--- js/cakerousel.js
+++ js/cakerousel.js
@@ -20,111 +20,115 @@ startCarousel('#chococake',testValues )
                 firstSlice:firstSlice,
                 lastSlice:lastSlice
             };
-
+            autoPlay(carouselID, valuesObject, objSlices, totalWidth);
             if($(carouselID+' .cakerousel-slider').hasClass('auto')){
                 window.addEventListener('focus', function() {
-                    console.log(carouselID+' active');
+                    console.log('active tab');
+                    stopAutoPlay(valuesObject);
                     autoPlay(carouselID, valuesObject, objSlices, totalWidth);
                 },false);
 
                 window.addEventListener('blur', function() {
-                    console.log(carouselID+' inactive');
+                    console.log('inactive tab');
                     stopAutoPlay(valuesObject);
                 },false);
             }
 
             $(carouselID+' .cakerousel-control-r').on('click', function(){
                 nextSlice(carouselID, valuesObject, objSlices, totalWidth);
+                console.log('R', $('.cakerousel-element'))
+
             });
 
             $(carouselID+' .cakerousel-control-l').on('click', function(){
                 previousSlice(carouselID, valuesObject, objSlices);
+                console.log('L', $('.cakerousel-element'))
+
             });
         }
     }
 
+    function autoPlay(carouselID, obj, objSlices, totalWidth){
+        console.log('autoplay ON');
+        obj.timer = setInterval(function(){
+            nextSlice(carouselID, obj, objSlices, totalWidth);
+        }, obj.autoScrollTime);
+    }
+    function stopAutoPlay(obj) {
+        console.log('autoplay OFF');
+        clearInterval(obj.timer);
+    }
 
-function autoPlay(carouselID, obj, objSlices, totalWidth){
-    obj.timer = setInterval(function(){
-        nextSlice(carouselID, obj, objSlices, totalWidth);
-            }, obj.autoScrollTime);
-}
+    function nextSlice(carouselID, obj, objSlices, totalWidth){
+        if(obj.currentPos<totalWidth && obj.currentPosEnd+obj.scrollAmount<totalWidth){
+            obj.currentPosEnd+=obj.scrollAmount;
+            obj.currentPos+=obj.scrollAmount;
+            $(carouselID+' .cakerousel-elements').animate({scrollLeft:obj.currentPos},obj.speedScroll);
+        } else if($(carouselID+' .cakerousel-slider').hasClass('loop')) {
+            fromLastToFirst(carouselID, obj, objSlices);
+        } else {
+            jumpToFirst(carouselID, obj)
+        }
+    }
 
-function stopAutoPlay(obj) {
-    clearInterval(obj.timer);
-}
+    function previousSlice(carouselID, obj, objSlices){
+        if(obj.currentPos>0){
+            obj.currentPosEnd-=obj.scrollAmount;
+            obj.currentPos-=obj.scrollAmount;
+            $(".cakerousel-elements").animate({scrollLeft:obj.currentPos},obj.speedScroll);
+        } else if($('.cakerousel-slider').hasClass('loop')) {
+            fromFirstToLast(carouselID, obj, objSlices)
+        } else {
+            jumpToLast(carouselID, obj);
+        }
+    }
 
-function nextSlice(carouselID, obj, objSlices, totalWidth){
-    if(obj.currentPos<totalWidth && obj.currentPosEnd+obj.scrollAmount<totalWidth){
+    function jumpToFirst(carouselID, obj){
+        obj.currentPos=0;
+        obj.currentPosEnd=obj.scrollAmount;
+        $(carouselID+' .cakerousel-elements').animate({scrollLeft:obj.currentPos},obj.speedScroll);
+    }
+
+    function jumpToLast(carouselID, obj){
+        obj.currentPos=(obj.scrollAmount*(getQuantityCarouselElements(carouselID)-1));
+        obj.currentPosEnd=(obj.scrollAmount*getQuantityCarouselElements(carouselID))
+        $(carouselID+' .cakerousel-elements').animate({scrollLeft:obj.currentPos},obj.speedScroll);
+    }
+
+    function getQuantityCarouselElements(carouselID){
+        return $(carouselID+' .cakerousel-element').length;
+    }
+
+    function getTotalWidth(carouselID){
+        var totalWidth=0;
+        for(var i = 0;  i < $(carouselID+' .cakerousel-element').length; i++){
+            totalWidth+=$($(carouselID+' .cakerousel-element')[i]).width()
+        }
+
+        return totalWidth+80;
+    }
+
+    function fromLastToFirst(carouselID, obj, objSlices){
+        $(objSlices.firstSlice).insertAfter(objSlices.lastSlice);
+        obj.currentPosEnd-=obj.scrollAmount;
+        obj.currentPos-=obj.scrollAmount;
+        $(carouselID+' .cakerousel-elements').animate({scrollLeft:obj.currentPos},0);
+        objSlices.firstSlice = $(carouselID+' .cakerousel-element')[0];
+        objSlices.lastSlice = $(carouselID+' .cakerousel-element')[($(carouselID+' .cakerousel-element').length-1)];
         obj.currentPosEnd+=obj.scrollAmount;
         obj.currentPos+=obj.scrollAmount;
         $(carouselID+' .cakerousel-elements').animate({scrollLeft:obj.currentPos},obj.speedScroll);
-    } else if($(carouselID+' .cakerousel-slider').hasClass('loop')) {
-        fromLastToFirst(carouselID, obj, objSlices);
-    } else {
-        jumpToFirst(carouselID, obj)
     }
-}
 
-function previousSlice(carouselID, obj, objSlices){
-    if(obj.currentPos>0){
+    function fromFirstToLast(carouselID, obj, objSlices){
+        $(objSlices.lastSlice).insertBefore(objSlices.firstSlice);
+        obj.currentPosEnd+=obj.scrollAmount;
+        obj.currentPos+=obj.scrollAmount;
+        $(carouselID+' .cakerousel-elements').animate({scrollLeft:obj.currentPos},0);
+        objSlices.firstSlice = $(carouselID+' .cakerousel-element')[0];
+        objSlices.lastSlice = $(carouselID+' .cakerousel-element')[($(carouselID+' .cakerousel-element').length-1)];
         obj.currentPosEnd-=obj.scrollAmount;
         obj.currentPos-=obj.scrollAmount;
-        $(".cakerousel-elements").animate({scrollLeft:obj.currentPos},obj.speedScroll);
-    } else if($('.cakerousel-slider').hasClass('loop')) {
-        fromFirstToLast(carouselID, obj, objSlices)
-    } else {
-        jumpToLast(carouselID, obj);
+        $(carouselID+' .cakerousel-elements').animate({scrollLeft:obj.currentPos},obj.speedScroll);
     }
-}
-
-function jumpToFirst(carouselID, obj){
-    obj.currentPos=0;
-    obj.currentPosEnd=obj.scrollAmount;
-    $(carouselID+' .cakerousel-elements').animate({scrollLeft:obj.currentPos},obj.speedScroll);
-}
-
-function jumpToLast(carouselID, obj){
-    obj.currentPos=(obj.scrollAmount*(getQuantityCarouselElements(carouselID)-1));
-    obj.currentPosEnd=(obj.scrollAmount*getQuantityCarouselElements(carouselID))
-    $(carouselID+' .cakerousel-elements').animate({scrollLeft:obj.currentPos},obj.speedScroll);
-}
-
-function getQuantityCarouselElements(carouselID){
-    return $(carouselID+' .cakerousel-element').length;
-}
-
-function getTotalWidth(carouselID){
-    var totalWidth=0;
-    for(var i = 0;  i < $(carouselID+' .cakerousel-element').length; i++){
-        totalWidth+=$($(carouselID+' .cakerousel-element')[i]).width()
-    }
-
-    return totalWidth+80;
-}
-
-function fromLastToFirst(carouselID, obj, objSlices){
-    $(objSlices.firstSlice).insertAfter(objSlices.lastSlice);
-    obj.currentPosEnd-=obj.scrollAmount;
-    obj.currentPos-=obj.scrollAmount;
-    $(carouselID+' .cakerousel-elements').animate({scrollLeft:obj.currentPos},0);
-    objSlices.firstSlice = $(carouselID+' .cakerousel-element')[0];
-    objSlices.lastSlice = $(carouselID+' .cakerousel-element')[($(carouselID+' .cakerousel-element').length-1)];
-    obj.currentPosEnd+=obj.scrollAmount;
-    obj.currentPos+=obj.scrollAmount;
-    $(carouselID+' .cakerousel-elements').animate({scrollLeft:obj.currentPos},obj.speedScroll);
-}
-
-function fromFirstToLast(carouselID, obj, objSlices){
-    $(objSlices.lastSlice).insertBefore(objSlices.firstSlice);
-    obj.currentPosEnd+=obj.scrollAmount;
-    obj.currentPos+=obj.scrollAmount;
-    $(carouselID+' .cakerousel-elements').animate({scrollLeft:obj.currentPos},0);
-    objSlices.firstSlice = $(carouselID+' .cakerousel-element')[0];
-    objSlices.lastSlice = $(carouselID+' .cakerousel-element')[($(carouselID+' .cakerousel-element').length-1)];
-    obj.currentPosEnd-=obj.scrollAmount;
-    obj.currentPos-=obj.scrollAmount;
-    $(carouselID+' .cakerousel-elements').animate({scrollLeft:obj.currentPos},obj.speedScroll);
-}
-
 });
